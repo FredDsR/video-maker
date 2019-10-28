0af3d1d69eb7fbad0ec1a1cfadb9af5417ad508f
Frederico Reckziegel <freddalsoglio@gmail.com>
2019-10-28 17:44:51 -0300

    feat: add video robot and convert image

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const robots = {
     state: require('./robots/state.js'),
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    image: require('./robots/image.js')
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js')
 }
 
 async function start() {
     robots.input()
     await robots.text()
     await robots.image()
-
-    console.log(' >> The process is complete.')
-    const content = robots.state.load()
-    console.dir(content, { depth: null })
+    await robots.video()
+    // const content = robots.state.load()
+    // console.dir(content, { depth: null })
 }
 
 start()
